f77c2d2953a915dbd0fdca5a1aa8656b1b1b533a
bluelife <28998638@qq.com>
2016-04-27 02:53:22 +0800

    Initial commit

---
+++ app/src/main/java/com/bluelife/test/intformattest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bluelife.test.intformattest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Integer.parseInt("12.3");
+    }
+}
